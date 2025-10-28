4207e1c4ec3ec0d15744a543ede59d743e28acfa
Nikita L <logach3@gmail.com>
2025-10-28 12:05:24 +0300

    upd: remove strategy WithdrawalRequest.sol

--- src/strategy/WithdrawalRequest.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.25;
-
-struct WithdrawalRequest {
-    bytes32 strategyRequestId;
-    address owner;
-    uint256 stethAmount;
-    uint40 timestamp;
-}
