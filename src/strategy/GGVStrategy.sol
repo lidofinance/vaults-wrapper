735b8d554ecf7ad7ea1055263e9fef54a2187e7e
Nikita L <logach3@gmail.com>
2025-12-09 15:47:30 +0300

    fix ggv

--- src/strategy/GGVStrategy.sol
+++ src/strategy/GGVStrategy.sol
@@ -5,7 +5,6 @@ import {
     AccessControlEnumerableUpgradeable
 } from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
@@ -22,7 +21,6 @@ import {IWstETH} from "src/interfaces/core/IWstETH.sol";
 
 contract GGVStrategy is IStrategy, AccessControlEnumerableUpgradeable, FeaturePausable, StrategyCallForwarderRegistry {
     using SafeCast for uint256;
-    using SafeERC20 for IERC20;
 
     StvStETHPool private immutable POOL_;
     IWstETH public immutable WSTETH;
