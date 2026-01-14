c2235b68a2ec1d473657a7127edb50e3c1b6f7d9
Nikita L <logach3@gmail.com>
2026-01-14 13:54:34 +0300

    fix: ggvstrategy wsteth validation

--- src/strategy/GGVStrategy.sol
+++ src/strategy/GGVStrategy.sol
@@ -179,6 +179,7 @@ contract GGVStrategy is IStrategy, AccessControlEnumerableUpgradeable, FeaturePa
      */
     function requestExitByWsteth(uint256 _wsteth, bytes calldata _params) external returns (bytes32 requestId) {
         if (_wsteth == 0) revert ZeroArgument("_wsteth");
+        
         GGVParamsRequestExit memory params = abi.decode(_params, (GGVParamsRequestExit));
 
         IStrategyCallForwarder callForwarder = _getOrCreateCallForwarder(msg.sender);
